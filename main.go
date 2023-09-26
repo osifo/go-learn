6df6ba1d9ddf8ffa70bf0753cf42e0ef6e165ed3
Anosike Osifo <code.anosikeosifo@gmail.com>
2023-09-26 12:16:08 +0200

    file io

--- main.go
+++ main.go
@@ -1,18 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main() {
-	print("This is a Go function")
+	"osifo.dev/learn-go/fileutils"
+)
 
-	learnDefer()
-	UpdateWeddingDetails()
-	RunCalculator()
+func readFileData() {
+	content, err := fileutils.ReadTextfile("fileutils/data/user.json")
+
+	if err != nil {
+		fmt.Printf("An error occurred while reading file: \n %s", err)
+	} else {
+		fmt.Printf("\n %s \n", content)
+	}
+}
+
+func writeDateToFile() {
+	error := fileutils.WriteToFile("fileutils/data/output.txt", "this is a test output")
+
+	if error != nil {
+		fmt.Printf("Something went wrong while writing data to specified file:\n%s", error)
+	} else {
+		fmt.Println("Data written to file succesfully")
+	}
 }
 
 func learnDefer() {
 	var age int32 = 22
 
 	defer fmt.Println("this is a deferred statement")
-	fmt.Println(age, "\n")
+	fmt.Println(age)
+}
+
+func main() {
+	print("This is a Go function")
+	learnDefer()
+	UpdateWeddingDetails()
+	// RunCalculator()
+	readFileData()
+	writeDateToFile()
 }
